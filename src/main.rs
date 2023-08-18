6eb29b11bebccb4ea3a48238a2324809e08bd8c8
Shnob <jake.dombnz@gmail.com>
2023-08-18 23:50:46 +1200

    Switched from files to folders, I kinda forgot what I was doing
    Now the files are in a list, with the selected being highlighted
    Still working on the user input for switching these
    The preview is now all the files/folders in the folder
    The preview window is a litte smaller

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,7 @@ fn restore_terminal(
 
 fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn Error>> {
     let bookmarks_vec = read_lines("bookmarks.txt")?;
+    let mut selected_folder = 0;
 
     Ok(loop {
         terminal.draw(|frame| {
@@ -50,33 +51,56 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn 
             let horiz_chunks = Layout::default()
                 .direction(Direction::Horizontal)
                 .margin(0)
-                .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+                .constraints([Constraint::Percentage(65), Constraint::Percentage(35)].as_ref())
                 .split(vert_chunks[0]);
 
-            let file_names = Paragraph::new(bookmarks_vec.join("\n"))
-                .block(Block::default().title("Bookmarks").borders(Borders::ALL));
-            frame.render_widget(file_names, horiz_chunks[0]);
+            let folder_names = List::new(
+                bookmarks_vec
+                    .iter()
+                    .enumerate()
+                    .map(|(i, x)| (i, ListItem::new(x.as_str())))
+                    .map(|(i, x)| {
+                        if i == selected_folder {
+                            x.style(Style::default().fg(Color::Black).bg(Color::White))
+                        } else {
+                            x
+                        }
+                    })
+                    .collect::<Vec<ListItem>>(),
+            )
+            .block(Block::default().title("Bookmarks").borders(Borders::ALL));
+            frame.render_widget(folder_names, horiz_chunks[0]);
 
-            let selected_file = bookmarks_vec.get(0).unwrap();
+            let selected_folder = bookmarks_vec.get(selected_folder).unwrap();
 
-            let file_previews = Paragraph::new(get_file_preview(selected_file))
-                .block(Block::default().title("File preview").borders(Borders::ALL));
-            frame.render_widget(file_previews, horiz_chunks[1]);
+            let folder_previews = Paragraph::new(get_folder_preview(selected_folder)).block(
+                Block::default()
+                    .title("Folder preview")
+                    .borders(Borders::ALL),
+            );
+            frame.render_widget(folder_previews, horiz_chunks[1]);
 
             let search_bar = Paragraph::new(" -> ").block(Block::default().borders(Borders::ALL));
             frame.render_widget(search_bar, vert_chunks[1]);
         })?;
-        if event::poll(Duration::from_millis(250))? {
+        if event::poll(Duration::from_millis(50))? {
             if let Event::Key(key) = event::read()? {
-                if KeyCode::Char('q') == key.code {
-                    break;
+                match key.code {
+                    KeyCode::Char('q') => break,
+                    // BUG: This stuff don't work
+                    KeyCode::Up | KeyCode::Right => {selected_folder = (selected_folder - 1).wrapping_rem_euclid(bookmarks_vec.len())},
+                    KeyCode::Down | KeyCode::Left => {selected_folder = (selected_folder + 1).rem_euclid(bookmarks_vec.len())},
+                    _ => (),
                 }
+                //if KeyCode::Char('q') == key.code {
+                //    break;
+                //}
             }
         }
     })
 }
 
-fn get_file_preview(filename: &str) -> String {
+fn get_folder_preview(filename: &str) -> String {
     let ps = PsScriptBuilder::new()
         .no_profile(true)
         .non_interactive(true)
@@ -84,7 +108,7 @@ fn get_file_preview(filename: &str) -> String {
         .print_commands(false)
         .build();
 
-    let output = ps.run(&format!(r#"cat "{}""#, filename));
+    let output = ps.run(&format!(r#"ls -Name -Force "{}""#, filename));
 
     match output {
         Ok(output) => output.stdout().unwrap(),
