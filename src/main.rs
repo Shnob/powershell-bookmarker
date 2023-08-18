6664308388b9830cfb4cf2998a916cf1fe6c176c
Shnob <jake.dombnz@gmail.com>
2023-08-18 22:21:36 +1200

    Basic layout present

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use crossterm::{
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use ratatui::{prelude::*, widgets::Paragraph};
+use ratatui::{prelude::*, widgets::*};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut terminal = setup_terminal()?;
@@ -36,8 +36,28 @@ fn restore_terminal(
 fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn Error>> {
     Ok(loop {
         terminal.draw(|frame| {
-            let greeting = Paragraph::new("Hello World!");
-            frame.render_widget(greeting, frame.size());
+            let vert_chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(0)
+                .constraints([Constraint::Min(3), Constraint::Length(3)].as_ref())
+                .split(frame.size());
+
+            let horiz_chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .margin(0)
+                .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+                .split(vert_chunks[0]);
+
+            let file_names = Paragraph::new("Files go here")
+                .block(Block::default().title("Bookmarks").borders(Borders::ALL));
+            frame.render_widget(file_names, horiz_chunks[0]);
+
+            let file_previews = Paragraph::new("Preview goes here")
+                .block(Block::default().title("File preview").borders(Borders::ALL));
+            frame.render_widget(file_previews, horiz_chunks[1]);
+
+            let search_bar = Paragraph::new(" -> ").block(Block::default().borders(Borders::ALL));
+            frame.render_widget(search_bar, vert_chunks[1]);
         })?;
         if event::poll(Duration::from_millis(250))? {
             if let Event::Key(key) = event::read()? {
