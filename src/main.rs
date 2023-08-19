deacd45368e1ef5f866701e4f9cade2f99334cc5
Shnob <jake.dombnz@gmail.com>
2023-08-19 18:04:31 +1200

    Folder previews are only loaded once each, they are also loaded in a much faster way

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use std::{
     error::Error,
-    fs::{File, self},
+    fs::{self, File},
     io::{self, BufRead, BufReader, Stdout},
     path::Path,
-    time::Duration,
 };
 
 use crossterm::{
@@ -11,7 +10,6 @@ use crossterm::{
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use powershell_script::PsScriptBuilder;
 use ratatui::{prelude::*, widgets::*};
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -21,7 +19,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let choice = choice.unwrap_or_else(|| ".".to_string());
 
-    fs::write("C:/Users/Jake/AppData/Local/Temp/powershell_script_temp_file.txt", choice).unwrap();
+    fs::write(
+        "C:/Users/Jake/AppData/Local/Temp/powershell_script_temp_file.txt",
+        choice,
+    )
+    .unwrap();
 
     Ok(())
 }
@@ -51,6 +53,7 @@ fn run(
 ) -> Result<Option<String>, Box<dyn Error>> {
     let bookmarks_vec = read_lines("bookmarks.txt")?;
     let mut selected_index: usize = 0;
+    let mut folder_preview = String::new();
     let mut user_search = String::new();
     let mut ui_mode = UiMode::Search;
 
@@ -85,9 +88,7 @@ fn run(
             .block(Block::default().title("Bookmarks").borders(Borders::ALL));
             frame.render_widget(folder_names, horiz_chunks[0]);
 
-            let selected_folder = bookmarks_vec.get(selected_index).unwrap();
-
-            let folder_previews = Paragraph::new(get_folder_preview(selected_folder)).block(
+            let folder_previews = Paragraph::new(folder_preview.clone()).block(
                 Block::default()
                     .title("Folder preview")
                     .borders(Borders::ALL),
@@ -99,45 +100,47 @@ fn run(
             frame.render_widget(search_bar, vert_chunks[1]);
         })?;
 
-        if event::poll(Duration::from_millis(10))? {
-            if let Event::Key(key) = event::read()? {
-                if let KeyEventKind::Release = key.kind {
-                    continue;
-                }
-                match ui_mode {
-                    UiMode::Normal => match key.code {
-                        KeyCode::Char('q') => break,
-                        KeyCode::Up | KeyCode::Right | KeyCode::Char('k') => {
-                            selected_index = (selected_index as isize - 1)
-                                .rem_euclid(bookmarks_vec.len() as isize)
-                                as usize;
-                        }
-                        KeyCode::Down | KeyCode::Left | KeyCode::Char('j') => {
-                            selected_index = (selected_index as isize + 1)
-                                .rem_euclid(bookmarks_vec.len() as isize)
-                                as usize;
-                        }
-                        KeyCode::Enter => {
-                            return Ok(Some(bookmarks_vec[selected_index].clone()));
-                        }
-                        _ => (),
-                    },
-                    UiMode::Search => match key.code {
-                        KeyCode::Esc => {
-                            ui_mode = UiMode::Normal;
-                        }
-                        KeyCode::Backspace => {
-                            user_search.pop();
-                        }
-                        KeyCode::Char(c) => {
-                            user_search = format!("{}{}", user_search, c);
-                        }
-                        KeyCode::Enter => {
-                            return Ok(Some(bookmarks_vec[selected_index].clone()));
-                        }
-                        _ => (),
-                    },
-                }
+        if let Event::Key(key) = event::read()? {
+            if let KeyEventKind::Release = key.kind {
+                continue;
+            }
+            match ui_mode {
+                UiMode::Normal => match key.code {
+                    KeyCode::Char('q') => break,
+                    KeyCode::Up | KeyCode::Right | KeyCode::Char('k') => {
+                        selected_index = (selected_index as isize - 1)
+                            .rem_euclid(bookmarks_vec.len() as isize)
+                            as usize;
+                        folder_preview =
+                            get_folder_preview(bookmarks_vec.get(selected_index).unwrap());
+                    }
+                    KeyCode::Down | KeyCode::Left | KeyCode::Char('j') => {
+                        selected_index = (selected_index as isize + 1)
+                            .rem_euclid(bookmarks_vec.len() as isize)
+                            as usize;
+                        folder_preview =
+                            get_folder_preview(bookmarks_vec.get(selected_index).unwrap());
+                    }
+                    KeyCode::Enter => {
+                        return Ok(Some(bookmarks_vec[selected_index].clone()));
+                    }
+                    _ => (),
+                },
+                UiMode::Search => match key.code {
+                    KeyCode::Esc => {
+                        ui_mode = UiMode::Normal;
+                    }
+                    KeyCode::Backspace => {
+                        user_search.pop();
+                    }
+                    KeyCode::Char(c) => {
+                        user_search.push(c);
+                    }
+                    KeyCode::Enter => {
+                        return Ok(Some(bookmarks_vec[selected_index].clone()));
+                    }
+                    _ => (),
+                },
             }
         }
     }
@@ -145,19 +148,19 @@ fn run(
 }
 
 fn get_folder_preview(filename: &str) -> String {
-    let ps = PsScriptBuilder::new()
-        .no_profile(true)
-        .non_interactive(true)
-        .hidden(false)
-        .print_commands(false)
-        .build();
+    let paths = fs::read_dir(filename).unwrap();
 
-    let output = ps.run(&format!(r#"ls -Name -Force "{}""#, filename));
+    let mut file_list = String::new();
 
-    match output {
-        Ok(output) => output.stdout().unwrap(),
-        Err(e) => panic!("{}", e),
+    for path in paths {
+        let file = path.unwrap();
+        let file_name = file.file_name();
+        let file_name = file_name.to_string_lossy();
+        file_list.push_str(&file_name);
+        file_list.push('\n');
     }
+
+    file_list
 }
 
 fn read_lines<P>(filename: P) -> io::Result<Vec<String>>
